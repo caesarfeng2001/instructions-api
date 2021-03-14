088c2e4fb6b8c7bdcc7c531247ffa7bf486a0d25
Caesar Feng <caesar@rcafinnovation.ca>
2021-03-13 20:06:09 -0500

    init

--- Startup.cs
+++ Startup.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using AutoMapper;
+using AutoMapper; 
 using Commander.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
